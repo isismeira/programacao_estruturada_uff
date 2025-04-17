4a892799d3e09a52895124db0c903823d5290cc1
Isis Meira <167538407+isismeira@users.noreply.github.com>
2025-04-17 15:16:38 -0300

    Commit

--- exercicios_aline/slide_4/quicksort.c
+++ exercicios_aline/slide_4/quicksort.c
@@ -1,52 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void troca(int array[], int a, int b);
-int particao(int array[], int inicio, int fim);
-void quicksort(int array[], int inicio, int fim);
+void quicksort(int *vetor, int inicio, int fim);
+int particiona(int *vetor, int inicio, int fim);
 
-// Função principal
-int main() {
-    int array[] = {8, 3, 1, 7, 0, 10, 2};
-    int len = sizeof(array) / sizeof(array[0]);
+int main()
+{
+    int vetor[8] = {22, 45, 13, 30, 37, 34, 48, 20};
+    int len = sizeof(vetor)/sizeof(vetor[0]);
 
-    quicksort(array, 0, len - 1);
+    quicksort(vetor, 0, len - 1);
 
-    printf("Ordenacao por quicksort: \n");
     for (int i = 0; i < len; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
-
-// Função Quicksort recursiva
-void quicksort(int array[], int inicio, int fim) {
-    if (inicio< fim) {
-        int indicePivo = particao(array, inicio, fim);
-        quicksort(array, inicio, indicePivo - 1);  // Lado esquerdo
-        quicksort(array, indicePivo + 1, fim);    // Lado direito
+        printf("%i ", vetor[i]);
     }
 }
 
-// Função de partição (estilo Lomuto)
-int particao(int array[], int inicio, int fim) {
-    int pivo = array[fim];  // O pivô é sempre o último elemento
-    int indicePivo = inicio;
-
-    for (int i = inicio; i < fim; i++) {
-        if (array[i] < pivo) {
-            troca(array, i, indicePivo);
-            indicePivo++;
-        }
+void quicksort(int *vetor, int inicio, int fim)
+{
+    if (inicio < fim) {
+        int posicao = particiona(vetor, inicio, fim); // retorna o indice do meio, o elemento que está na posição correta
+        quicksort(vetor, inicio, posicao - 1); // chama recursivamente a parte a esquerda do pivô (inicio - meio)
+        quicksort(vetor, posicao, fim); // chama recursivamente a parte a direita do pivô (meio - fim)
     }
-
-    troca(array, indicePivo, fim);  // Coloca o pivô na posição correta
-    return indicePivo;
 }
 
-void troca(int array[], int a, int b) {
-    int temp = array[a];
-    array[a] = array[b];
-    array[b] = temp;
+/* Vai colocar um elemento na posição correta (pivô), 
+onde todos os elementos a sua esquerda serão menores que ele, e a direita, maiores */
+int particiona(int *vetor, int inicio, int fim) 
+{
+    int pivo = (vetor[inicio] + vetor[fim] + vetor[(inicio+fim)/2]) / 3;
+
+    while (inicio < fim) { // enquanto os elementos i e f não se "cruzarem"  
+        while (inicio < fim && vetor[inicio] <= pivo) // percorre o vetor da esquerda para o meio, procurando o maior
+            inicio += 1; 
+        while (inicio < fim && vetor[fim] > pivo) // percorre o vetor da direita para o meio, procurando o menor
+            fim -= 1; 
+        
+        // Realiza a troca entre os elementos
+        int temp = vetor[inicio]; 
+        vetor[inicio] = vetor[fim];
+        vetor[fim] = temp;    
+    }
+
+    return inicio; // obs. aqui, inicio = fim
 }
